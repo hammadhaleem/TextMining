ba06fa278d5e0416ddc3b7bfefab85507cdc9392
hammad haleem <hammadhaleem@gmail.com>
2013-11-23 12:15:54 +0530

    final

--- Process/ipe.py
+++ Process/ipe.py
@@ -1,21 +1,19 @@
+import os
+import threading
+import time
+import _mysql,MySQLdb
+
 def getTextSet(p):
-#input pi = [[t1,f1],[t2,f2]...[tn,fn]]
 	l=[]
 	for i in p : 
 		l.append(i[0])
 	return l
-#output ti = { t1, t2 ,t3 ..tn }
-
-# Format [list of list] 
-#NDP =[ [[1,2],[2,3],[3]] , [[1,2],[2,3],[3]],  [[1,2],[2,3],[3]]]
-
 def getsum1(t,p):
 	sum1 = 0 
 	for i in t :
 		for j in p :	
 			if i == j:
 				sum1 =sum1+i[1]
-
 	return sum1 
 
 def getsum2(t,p,sum_intersection):
@@ -24,18 +22,11 @@ def getsum2(t,p,sum_intersection):
 	for i in p :
 		sum1 =sum1+i[1]
 	sum1 =sum1 - 2*sum_intersection
-
 	return sum1 
- 
 
 def remove_pattern(NDP,p):
-	new_NDP=[]
-	for pi in NDP : 
-		if pi is p :
-			print "Removed"
-		else :
-			new_NDP.append(pi)
-	return new_NDP 
+	NDP.remove(p)
+	return NDP
 
 def suffling(nd , Dnd , NDP,u ):
 	for p in Dnd :
@@ -59,18 +50,94 @@ def suffling(nd , Dnd , NDP,u ):
 				else
 					t[1] = t[1]*(1+offering / base ) 
 				l.append(t)
-			new_NDP.append(l)
-			
-			
+			new_NDP.append(l)			
 	return new_NDP 
 
+def Threadhold(DP):
+	value = 0 
+	return value
 
+def find_Dnd(nd, DP ):
+	Dnd = [] 
+	for term in nd :
+		for pattern in DP :
+			if term in pattern :
+				Dnd.append(pattern)
+				Dp.remove(pattern)
+	return Dnd 
 
+def composition ( np , pattern ):
+	d =dict() 
+	for term, weight in np :
+		d[term] = int (weight)  
+	for term,weight in pattern :
+		if d.has_key(term):
+			d[term] = int(d[term])+int(weight)
+		else : 
+			d[term] =int(weight)
+	return d
+def Threshold(DP):
+	min= 9999 
+	l =[]
+	for pattern in DP :
+		sum = 0 
+		for weight in pattern :
+			sum =sum +weight 
+		if sum < min :
+			min = sum 
+			l = pattern
+	return min
 
+def IPE(D,DP,u):
+	np = dict()
+	NDP = DP #in our case, we have already normalized all D patterns 
+	threshold =Threshold(DP)
+	for nd  in D[neg]:
+		if weight(nd) >= threshold:
+			Dnd = find_Dnd(nd,DP)
+			NDP = suffling(nd,Dnd,NDP,u)
+			for pattern in NDP : 
+				np = composition(np,pattern)
+	return np 
 
+# Format [list of list] 
+#NDP =[ [[1,2],[2,3],[3]] , [[1,2],[2,3],[3]],  [[1,2],[2,3],[3]]]
+def dict_to_list(dic):
+	l=[]
+	key = getkeys(dic)
+	for i in keys :
+		l.append([i,dic[i]])
+	return l 
+def convert (lis):
+	l= []
+	for i in lis : 
+		for j in i :
+			l = l.append(j)
+	return l 
+u=0.2
+D= dict()
+D['neg'] = []
+D['pos'] = []
+DP=[]
+connection = MySQLdb.connect('localhost', 'root', 'kgggdkp2692', 'mining')
+cursor = connection.cursor()
+query= "select * from data"
+q=cursor.execute(query)
+rows = cursor.fetchall()
+print q ,query
+connection.commit()
+connection.close()
 
+for row in rows:
+	if row['type'] == 'neg' : 
+		D['neg'].append (convert(row[data]))
+	else 
+		D['pos'].append(convert(row[data]))
+	if len(str(row['dp'])) > 2 :
+		dp = dp.append ( dict_to_list(row['dp'])) 
 
-						
+print D , dp 
+#IPE(D,DP,u)						
 				
 				
 	
