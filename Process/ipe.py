4a8e5b4bfbfdbf417d464524b7a5ab19cbb856e7
hammad haleem <hammadhaleem@gmail.com>
2013-11-23 20:33:44 +0530

    Ready for testing !

--- Process/ipe.py
+++ Process/ipe.py
@@ -1,143 +1,211 @@
 import os
 import threading
 import time
-import _mysql,MySQLdb
+import MySQLdb
 
 def getTextSet(p):
 	l=[]
-	for i in p : 
-		l.append(i[0])
+	for key,value  in p.iteritems():
+		l.append(key)
 	return l
 def getsum1(t,p):
-	sum1 = 0 
+	sum1 = 0.0
+	c= 0
 	for i in t :
-		for j in p :	
-			if i == j:
-				sum1 =sum1+i[1]
-	return sum1 
+		c+=1
+		for k,v in p.iteritems() :
+			
+			if i == k:
+				sum1 =sum1+v
+	return sum1*c
 
 def getsum2(t,p,sum_intersection):
-	for i in t : 
-		sum1 =sum1+i[1]
-	for i in p :
-		sum1 =sum1+i[1]
-	sum1 =sum1 - 2*sum_intersection
-	return sum1 
+	sum1 = 0.0 
+	c= 0 
+	for i,j in p.iteritems() :
+		sum1 =sum1+j
+		c+=1
+	
+	sum1 =2.0*sum1*c - sum_intersection
+	return sum1
 
 def remove_pattern(NDP,p):
-	NDP.remove(p)
+	try :
+		NDP.remove(p)
+	except :
+		pass
 	return NDP
 
 def suffling(nd , Dnd , NDP,u ):
+	new_NDP = []
 	for p in Dnd :
-		termset = getTextset(p)
+		termset = getTextSet(p)
 		flag = 1
-		for t in termset : 
+		for t in termset :
 			if t in nd :
-				flag = 0 
+				flag = 0
 			else :
-				flag = 1 
-		if fal is 0 :
-			new_NDP  = remove_pattern(NDP,p) #Remove complete confllict offenders 
-		else : 
-			var = getsum1( t, p ) 
-			offering = ( 1 - 1/u )* var 
-			base = getsum2(t,p,var)
-			l = [] 
-			for t in p :
-				if t in n :
-					t[1] = (1/u)*t[1] #shrink 
-				else
-					t[1] = t[1]*(1+offering / base ) 
-				l.append(t)
-			new_NDP.append(l)			
-	return new_NDP 
+				flag = 1
+		if flag is 0 :
+			new_NDP  = remove_pattern(NDP,p) #Remove complete confllict offenders
+		else :
+			var = 0.0
+			offering=0.0
+			base = 0.0
+			fl = 0.0
+			var = getsum1( termset, p )
+			fl = 1.0/u
+			offering = ( 1.0 - fl )* var
+			base = getsum2(t,p,var)			
+			l = []
+			
+			for key,val in p.iteritems() :
+				if key in nd :
+					p[key] = (1.0/u)*p[key] #shrink
+				else:
+					p[key] = p[key]*(1.0+offering / base )
+				l.append(p)
+			
+			new_NDP.append(l)
+	return new_NDP
 
 def Threadhold(DP):
-	value = 0 
+	value = 0
 	return value
 
 def find_Dnd(nd, DP ):
-	Dnd = [] 
+	Dnd = []
+
 	for term in nd :
 		for pattern in DP :
 			if term in pattern :
 				Dnd.append(pattern)
-				Dp.remove(pattern)
-	return Dnd 
+				DP.remove(pattern)
+	return Dnd
 
 def composition ( np , pattern ):
-	d =dict() 
+	d =dict()
 	for term, weight in np :
-		d[term] = int (weight)  
+		d[term] = int (weight)
 	for term,weight in pattern :
 		if d.has_key(term):
 			d[term] = int(d[term])+int(weight)
-		else : 
+		else :
 			d[term] =int(weight)
 	return d
 def Threshold(DP):
-	min= 9999 
+	min= 9999
 	l =[]
 	for pattern in DP :
-		sum = 0 
-		for weight in pattern :
-			sum =sum +weight 
+		sum = 0
+		for a,weight in pattern :
+			sum =sum +weight
 		if sum < min :
-			min = sum 
+			min = sum
 			l = pattern
 	return min
+def weight(list):
 
-def IPE(D,DP,u):
+	d = dict()
+	w=0
+	count = 0 
+	for word in list : 
+		count =count + 1 
+		try:
+			d[word]= d[word]+1
+		except:
+			d[word] = 1 
+	for key,value  in d.iteritems():
+		w=w+value
+	return w 
+
+def IPE(D,DP,u,threshold):
 	np = dict()
-	NDP = DP #in our case, we have already normalized all D patterns 
-	threshold =Threshold(DP)
-	for nd  in D[neg]:
+	NDP = DP #in our case, we have already normalized all D patterns
+	count = 0 
+	for v in D['neg']:
+		d = str(v)
+		nd = d.replace('[', '').replace("]", '').replace(' ','').split(",")
+		count =count + 1 
+		print "Counter:" , count  
 		if weight(nd) >= threshold:
 			Dnd = find_Dnd(nd,DP)
-			NDP = suffling(nd,Dnd,NDP,u)
-			for pattern in NDP : 
-				np = composition(np,pattern)
-	return np 
+			#print "Dnd",  Dnd 
+		NDP = suffling(nd,Dnd,NDP,u)
+		print weight(nd) , threshold
+		for pattern in NDP :
+			np = composition(np,pattern)
+		if (count % 20 )== 0 : 
+			print np 
+	return np
 
-# Format [list of list] 
+# Format [list of list]
 #NDP =[ [[1,2],[2,3],[3]] , [[1,2],[2,3],[3]],  [[1,2],[2,3],[3]]]
 def dict_to_list(dic):
-	l=[]
-	key = getkeys(dic)
-	for i in keys :
-		l.append([i,dic[i]])
-	return l 
+	k = []
+	for key,value  in dic.iteritems():
+		k.append([key,value])
+	return k
 def convert (lis):
-	l= []
-	for i in lis : 
+	lis1= []
+	for i in lis :
 		for j in i :
-			l = l.append(j)
-	return l 
-u=0.2
-D= dict()
-D['neg'] = []
-D['pos'] = []
-DP=[]
+			lis1.append(str(j))
+	return lis1
+
+
+
 connection = MySQLdb.connect('localhost', 'root', 'kgggdkp2692', 'mining')
 cursor = connection.cursor()
 query= "select * from data"
 q=cursor.execute(query)
 rows = cursor.fetchall()
-print q ,query
 connection.commit()
 connection.close()
 
-for row in rows:
-	if row['type'] == 'neg' : 
-		D['neg'].append (convert(row[data]))
-	else 
-		D['pos'].append(convert(row[data]))
-	if len(str(row['dp'])) > 2 :
-		dp = dp.append ( dict_to_list(row['dp'])) 
+import json
 
-print D , dp 
-#IPE(D,DP,u)						
-				
-				
+u=0.002
+D= dict()
+D['neg'] = []
+D['pos'] = []
+dp=[]
+
+threshold = 9999 
+for row in rows:	
+	try :
+		var = row[4].replace("'", "\"")
+		d= json.loads(var)
+	except :
+		d = ""
+
+	stri = row[3].replace('"', '').replace("'", '')
+	stri.split(' ')
+	if row[2] == 'neg' :
+		D['neg'].append (stri)
+	else:
+		D['pos'].append(stri)
+
+	if len(d) > 10 :
+		nd = stri.replace('[', '').replace("]", '').replace(' ','').split(",")
+		count = 0 
+		for i in nd :
+			count =count +1 
+		sum = 0.0
+		for key,value in d.iteritems():
+			c =value*count
+			sum = c + sum 
+		if sum <  threshold :
+			threshold = sum
+			print sum , threshold ,count
+		ls=d
+		dp.append(ls)
 	
+	#print  dp,D,u
+
+pattern = IPE(D,dp,20,threshold)
+print pattern
+
+
+
+
